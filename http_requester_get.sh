0de0e4d7685481de0fc01d4952492d20203a75c4
Masaharu S <30203831+natsuyoshi-jr@users.noreply.github.com>
2018-12-09 15:09:33 +0900

    Add http_reuester_get.sh

---
+++ http_requester_get.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+declare -r URL=$1
+declare -r PROXY_SERVER=''
+declare -r PROXY_PORT=''
+declare -r OUTPUT_FILE_NAME='api_response'
+declare -r COLOR_RED="\e[33;41;1m"
+declare -r COLOR_BLUE="\e[33;44;1m"
+declare -r COLOR_YELLOW="\e[31;43;1m"
+declare -r COLOR_OFF="\e[m"
+
+
+# Skip comment lines
+case "${URL}" in
+  http*)
+    :
+    ;;
+  *)
+    exit 0
+    ;;
+esac
+
+
+if [ -z "${PROXY_SERVER}" ]; then
+  # cURL
+  http_status_code=$(curl -s ${URL} -w "%{http_code}" -o ${OUTPUT_FILE_NAME})
+else
+  # cURL by using proxy
+  http_status_code=$(curl --proxy ${PROXY_SERVER}:${PROXY_PORT} -s ${URL} -w "%{http_code}" -o ${OUTPUT_FILE_NAME})
+fi
+
+if [ $? = 0 ]; then
+  case "${http_status_code}" in
+    2??)
+      echo -e "${COLOR_BLUE}[HTTP Status Code] ${http_status_code} ${COLOR_OFF}"
+      ;;
+    4??)
+      echo -e "${COLOR_RED}[HTTP Status Code] ${http_status_code} ${COLOR_OFF}"
+      ;;
+    *)
+      echo -e "${COLOR_YELLOW}[HTTP Status Code] ${http_status_code} ${COLOR_OFF}"
+      ;;
+  esac
+  
+  exit 0
+else
+  echo -e "${COLOR_RED}Failed to curl. ${COLOR_OFF}"
+  exit 1
+fi
